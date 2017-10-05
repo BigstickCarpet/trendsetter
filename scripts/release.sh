21690fd581ff52f22ac3eba6772b49e3bc1071de
James Messinger <james.messinger@gmail.com>
2017-10-05 14:01:06 -0500

    WIP: working on a release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
-# ====================================================================================
-# This script does a full release to production.  It runs linters and tests first,
-# then deploys the latest code, bumps the version number in AWS Lambda, package.json,
-# and Git, and updates the "prod" alias.
-# ====================================================================================
+# ==================================================================================================
+# This script does a full release to production.  It runs linters and tests first, then deploys the
+# latest code, bumps the version number in AWS Lambda, package.json, and Git, and updates the
+# "prod" alias.
+# ==================================================================================================
 
 # Stop on first error
 set -o errexit -o nounset -o pipefail
 
-npm run upgrade
-npm test
-npm run bump
+echo
+echo Updating all dependencies...
+npm run upgrade --silent
+
+echo Running ESLint...
+npm run lint --silent
+
+echo Running tests...
+npm test --silent
+
+# Bump the version number in package.json, Git, and AWS Lambda
+npm run bump --silent
+
