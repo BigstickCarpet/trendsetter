0e0d51cdb373a5582f09831a48fe8ca1a3022bba
James Messinger <james.messinger@gmail.com>
2017-10-07 21:19:22 -0500

    updated the website script

--- docs/js/trendsetter.js
+++ docs/js/trendsetter.js
@@ -11,62 +11,33 @@ $(function () {
   $.get(apiHost + '/trends')
     .done(function (data) {
       data.forEach(addTrendToTable);
-
-      if (data.length === 0) {
-        populateSampleData();
-      }
     })
     .fail(errorHandler);
 
   // Wire-up the "start a new trend" form
-  $('#new-trend').on('submit', function(event) {
-    event.preventDefault();
-    createTrend({
-      name: $('#name').val(),
-      from: parseInt($('#from').val()),
-      to: parseInt($('#to').val()),
+  $('#new-trend')
+    .attr('action', apiHost + '/trends')
+    .on('submit', function (event) {
+      event.preventDefault();
+      createTrend({
+        name: $('#name').val(),
+        type: $('#type').val(),
+        from: parseInt($('#from').val()),
+        to: parseInt($('#to').val()),
+      });
     });
-  });
 
   // Wire-up the "delete" buttons
-  $('#trends').on('click', '.btn-danger', function() {
+  $('#trends').on('click', '.btn-danger', function () {
     deleteTrend($(this).data('trend'));
   });
 
-  // Populate the API with sample trends
-  function populateSampleData() {
-    $.get('api/sample-data.json')
-      .done(function(data) {
-        data.forEach(createTrend);
-      })
-      .fail(errorHandler);
-  }
-
-  // Call the API to create a new trend
-  function createTrend(trend) {
-    $.post('api/trends', trend)
-      .done(function() {
-        setTimeout(function() {
-          location.reload();
-        }, 500);
-      })
-      .fail(errorHandler)
-  }
-
-  // Call the API to delete a trend
-  function deleteTrend(trend) {
-    $.ajax({ type: "DELETE", url: 'api/trends/' + encodeURIComponent(trend.name) })
-      .done(function() {
-        location.reload();
-      })
-      .fail(errorHandler)
-  }
-
   // Add the given trend to the <table>
-  function addTrendToTable(trend) {
+  function addTrendToTable (trend) {
     $('#loading').remove();
     $('#trends').append($('<tr>')
       .append($('<td></td>').text(trend.name))
+      .append($('<td></td>').text(trend.type))
       .append($('<td></td>').text(trend.from))
       .append($('<td></td>').text(trend.to))
       .append($('<td align="right"></td>')
@@ -77,11 +48,35 @@ $(function () {
     );
   }
 
+  // Call the API to create a new trend
+  function createTrend (trend) {
+    $.post(apiHost + '/trends', trend)
+      .done(function () {
+        setTimeout(function () {
+          location.reload();
+        }, 500);
+      })
+      .fail(errorHandler);
+  }
+
+  // Call the API to delete a trend
+  function deleteTrend (trend) {
+    $.ajax({ type: 'DELETE', url: apiHost + '/trends/' + trend.id })
+      .done(function () {
+        location.reload();
+      })
+      .fail(errorHandler);
+  }
+
   // HTTP error handler
-  function errorHandler(req) {
+  function errorHandler (req) {
     console.log(req);
-    var err = req.responseJSON || {error: req.status, message: req.responseText || req.statusText};
-    alert('HTTP ' + err.error + ' Error ' + req.statusText + ':\n\n' + err.message);
+    var err = req.responseJSON || {
+      error: req.status,
+      message: req.responseText || req.statusText,
+    };
+
+    alert('There was an HTTP ' + err.error + ' Error (' + req.statusText + '):\n\n' + err.message);
   }
 
 });
