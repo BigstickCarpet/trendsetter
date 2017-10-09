9d69523a9686b3ba6cd23dff85dcd5edab536372
James Messinger <james.messinger@gmail.com>
2017-10-08 20:35:25 -0500

    added the ability to filter trends by name (e.g. /trends?name=Furbies)

--- test/specs/findTrends.spec.js
+++ test/specs/findTrends.spec.js
@@ -67,6 +67,27 @@ describe('Find trends', () => {
       });
   });
 
+  it('returns only the trends match the name criteria', () => {
+    // Use a unique User ID, so we know there's no existing trends
+    let user = `${Date.now()}`;
+
+    let testTrends = [
+      { name: 'My Little Pony', type: 'Toys', from: 1983, to: 1992 },
+      { name: 'Arrested Development', type: 'TV Shows', from: 2003, to: 2006 },
+      { name: 'Arrested Development', type: 'TV Shows', from: 2013, to: 2013 },
+      { name: 'My Little Pony', type: 'Toys', from: 2010, to: 2014 },
+    ];
+
+    return testData.create(user, testTrends)
+      .then(() => apiGateway.auth(user).get('/trends?name=Arrested%20Development'))
+      .then(res => {
+        let trends = assert.isSuccessfulResponse(res, 200);
+        trends.should.be.an('array').with.lengthOf(2);
+        trends.forEach(assert.isValidTrend);
+        assert.matchesTestData(trends, [testTrends[1], testTrends[2]]);
+      });
+  });
+
   it('returns only the trends match the type criteria', () => {
     // Use a unique User ID, so we know there's no existing trends
     let user = `${Date.now()}`;
