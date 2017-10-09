6f3050657ba026de061d6372a86fdd50a1b30001
James Messinger <james.messinger@gmail.com>
2017-10-08 21:13:47 -0500

    Implemented the 10-year-buffer business rule

--- test/specs/createTrend.spec.js
+++ test/specs/createTrend.spec.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const apiGateway = require('../fixtures/apiGateway');
+const testData = require('../fixtures/testData');
 const assert = require('../fixtures/assert');
 
-describe.only('Create new trends', () => {
+describe('Create new trends', () => {
 
   // Create a unique User ID for each test, so we know we won't conflict with existing trends
   let user;
@@ -43,6 +44,54 @@ describe.only('Create new trends', () => {
         let trend = assert.isSuccessfulResponse(res, 201);
         assert.isValidTrend(trend);
         res.headers.should.have.property('Location', trend.links.self);
+      });
+  });
+
+  it('should return an error if the trend was popular within the past 10 years', () => {
+    let testTrends = [
+      { name: 'My Little Pony', type: 'Toys', from: 1983, to: 1992 },
+      { name: 'My Little Pony', type: 'Toys', from: 2010, to: 2014 },
+      { name: 'Arrested Development', type: 'TV Shows', from: 2003, to: 2006 },
+      { name: 'Arrested Development', type: 'TV Shows', from: 2013, to: 2013 },
+    ];
+
+    let newTrend = {
+      name: 'My Little Pony',
+      type: 'Different Type',
+      from: 2017,
+      to: 2018,
+    };
+
+    return testData.create(user, testTrends)
+      .then(() => apiGateway.auth(user).post('/trends', newTrend))
+      .then(res => {
+        let body = assert.isErrorResponse(res, 409);
+        body.error.should.equal('CONFLICT');
+        body.message.should.equal('My Little Pony was already trendy in 2014. It can\'t be trendy again until 2025.');
+      });
+  });
+
+  it('should return an error if the trend was popular within the next 10 years', () => {
+    let testTrends = [
+      { name: 'My Little Pony', type: 'Toys', from: 1983, to: 1992 },
+      { name: 'My Little Pony', type: 'Toys', from: 2010, to: 2014 },
+      { name: 'Arrested Development', type: 'TV Shows', from: 2003, to: 2006 },
+      { name: 'Arrested Development', type: 'TV Shows', from: 2013, to: 2013 },
+    ];
+
+    let newTrend = {
+      name: 'My Little Pony',
+      type: 'Different Type',
+      from: 1978,
+      to: 1980,
+    };
+
+    return testData.create(user, testTrends)
+      .then(() => apiGateway.auth(user).post('/trends', newTrend))
+      .then(res => {
+        let body = assert.isErrorResponse(res, 409);
+        body.error.should.equal('CONFLICT');
+        body.message.should.equal('My Little Pony was already trendy in 1983. It can\'t be trendy between 1972 and 1983.');
       });
   });
 
